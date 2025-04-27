b9714f32425ac29c4f25cfd3424b8ed1382673ba
samael pola <samaelpola06@gmail.com>
2025-04-27 22:31:46 +0200

    fix: update article model

--- src/models/article.model.js
+++ src/models/article.model.js
@@ -12,7 +12,7 @@ export const Article = sequelize.define("articles", {
     unique: true,
   },
   description: {
-    type: DataTypes.STRING,
+    type: DataTypes.TEXT,
     allowNull: false,
   },
   shortDescription: {
